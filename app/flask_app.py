e95601150e478a45d9234a77ef3da96897768307
dvkrr <dvkrrrr@gmail.com>
2020-05-05 16:28:31 +0300

    right

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template, request
-from flask_script import Manager, Command
+from flask import Flask, render_template, request, redirect
 import sqlite3
+from app.forms import LoginForm
+import os
+
+SECRET_KEY = os.urandom(64)
+
 app = Flask(__name__)
-manager = Manager(app)
+app.config['SECRET_KEY'] = SECRET_KEY
 app.debug = True
 
 
-
-
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    form = LoginForm()
     con = sqlite3.connect("bouquet.db")
     cur = con.cursor()
     result = cur.execute("""SELECT * FROM bouquets""").fetchall()
@@ -18,31 +20,9 @@ def index():
     result1 = cur.execute("""SELECT * FROM flowers""").fetchall()
     count1 = len(result1)
     result2 = cur.execute("""SELECT * FROM beauty""").fetchall()
-    count2  = len(result2)
-    return render_template('index.html', name=result, count=count, name1=result1, count1=count1, name2=result2, count2=count2)
-
-
-@app.route('/user/<int:user_id>/')
-def user_profile(user_id):
-    return "Profile page of user #{}".format(user_id)
-
-
-@app.route('/send.php')
-def books():
-    return "Hi"
-
-
-@app.route('/login/', methods=['post', 'get'])
-def login():
-    con = sqlite3.connect("bouquet.db")
-    cur = con.cursor()
-    result = cur.execute("""SELECT * FROM bouquets""").fetchall()
-    count = len(result)
-    return render_template('login.html', name=result, count=count)
-
-
-
-
+    count2 = len(result2)
+    return render_template('index.html', name=result, count=count, name1=result1, count1=count1, name2=result2,
+                           count2=count2, form=form)
 
 
 if __name__ == "__main__":
--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, SubmitField, TextAreaField, DateField
+from wtforms.validators import DataRequired
+
 
 
 class LoginForm(FlaskForm):
     name = StringField("Name: ", validators=[DataRequired()])
-    email = StringField("Email: ", validators=[Email()])
-    date = StringField("Date:", validators=[DataRequired()])
+    email = StringField("Email: ", validators=[DataRequired()])
+    date = DateField("Date and Time", validators=[DataRequired()])
     submit = SubmitField("Okay, BOOMER")
